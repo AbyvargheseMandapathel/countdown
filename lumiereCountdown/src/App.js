e3bdd0fd98112a15ecb4c2744c33f317a80542ed
Aby varghese <56151184+AbyvargheseMandapathel@users.noreply.github.com>
2023-03-09 13:03:27 +0530

    Add files via upload

---
+++ lumiereCountdown/src/App.js
@@ -0,0 +1,12 @@
+// new component
+import ComingSoon from './components/comingSoon/ComingSoon';
+
+function App() {
+  return (
+    <div className="background">
+      <ComingSoon/>
+    </div>
+  );
+}
+
+export default App;
